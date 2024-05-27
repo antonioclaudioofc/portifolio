332d5321567f782f71c64a0433f8da9dae78f416
Antonio Claudio <claudio1419teixeira2020@gmail.com>
2024-05-27 16:06:25 -0300

    feat: reponsive navbar

--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -1,33 +1,41 @@
 import Image from "next/image";
 import profile from "@/assets/perfil.png";
+import clsx from "clsx";
 
 export default function Main() {
   return (
-    <main className="bg-outer-space flex items-center flex-wrap">
-      <div className="max-w-2xl text-alabaster grid gap-4 px-24">
-        <p className="text-lg">Olá!</p>
-        <h1 className="text-6xl">Eu sou Antonio Claudio</h1>
-        <h2 className="text-4xl">Desenvolver FrontEnd</h2>
-        <p className="text-gray-200 font-bold text-base">
-          Entusiasta da tecnologia, especializado em transformar ideias
-          complexas em interfaces de usuário intuitivas
-        </p>
-        <div className="flex gap-4">
-          <button className="bg-big-foot-feel text-gray-50 px-8 py-4 font-bold rounded-md">
-            Fale comigo
-          </button>
-          <button className="text-big-foot-feel bg-gray-50 px-8 py-4 font-bold rounded-md">
-            Saiba mais
-          </button>
+    <main className="bg-outer-space">
+      <div
+        className={clsx(
+          "flex items-center justify-center flex-wrap px-24",
+          "max-xl:px-8 max-xl:w-full"
+        )}
+      >
+        <div className="text-alabaster flex flex-col gap-4 ">
+          <p className="text-lg">Olá!</p>
+          <h1 className="text-6xl">Eu sou Antonio Claudio</h1>
+          <h2 className="text-4xl">Desenvolver FrontEnd</h2>
+          <p className="text-gray-200 font-bold text-base max-w-lg">
+            Entusiasta da tecnologia, especializado em transformar ideias
+            complexas em interfaces de usuário intuitivas
+          </p>
+          <div className="flex gap-4">
+            <button className="bg-big-foot-feel text-gray-50 px-8 py-4 font-bold rounded-md">
+              Fale comigo
+            </button>
+            <button className="text-big-foot-feel bg-gray-50 px-8 py-4 font-bold rounded-md">
+              Saiba mais
+            </button>
+          </div>
         </div>
+        <Image
+          className="max-w-2xl"
+          src={profile}
+          width={500}
+          height={123}
+          alt=""
+        />
       </div>
-      <Image
-        className="max-w-2xl"
-        src={profile}
-        width={500}
-        height={123}
-        alt=""
-      />
     </main>
   );
 }
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,35 +1,113 @@
 "use client";
-import { Code } from "@phosphor-icons/react";
+
+import clsx from "clsx";
+import { useScrollPosition } from "../utils/useScrollPosition";
+import { Code, List, X } from "@phosphor-icons/react";
+import { useState } from "react";
+
+interface Options {
+  title: string;
+  url: string;
+}
 
 export default function Navbar() {
+  const scrollPosition = useScrollPosition();
+  const options: Options[] = [
+    { title: "Inicio", url: "#" },
+    { title: "Sobre mim", url: "#" },
+    { title: "Skills", url: "#" },
+    { title: "Meus Projetos", url: "3" },
+    { title: "Contato", url: "#" },
+  ];
   return (
-    <nav className="flex items-center justify-between px-24 pt-8 bg-outer-space">
-      <div className="flex gap-6">
-        <Code size={40} className="text-big-foot-feel w-10" />
-        <span className="text-alabaster text-3xl">Claudio</span>
-      </div>
-      <div className="flex items-center gap-6 text-alabaster">
-          <ul className="flex  text-base gap-6">
-            <li>
-              <a href="#">Inicio</a>
-            </li>
-            <li>
-              <a href="#">Sobre mim</a>
-            </li>
-            <li>
-              <a href="#">Skills</a>
-            </li>
-            <li>
-              <a href="#">Meus projetos</a>
-            </li>
-            <li>
-              <a href="#">Contatos</a>
-            </li>
+    <>
+      <nav
+        className={clsx(
+          scrollPosition > 0 ? "shadow-[0_4px_20px_-1px_rgba(0,0,0,0.1)]" : "",
+          "flex items-center justify-center sticky top-0 h-20 max-xl:h-20 bg-outer-space text-white transition-all duration-300"
+        )}
+      >
+        <div
+          className={clsx(
+            "flex justify-between px-24 w-full max-w-7xl items-center",
+            "max-xl:px-8"
+          )}
+        >
+          <div className="flex items-center gap-x-2">
+            <Code size={44} className="h-12 w-12 text-big-foot-feel" />
+            <h2 className="text-2xl font-bold">Claudio</h2>
+          </div>
+          <MenuHamburger options={options} />
+          <ul
+            className={clsx(
+              "flex items-center gap-6 text-white text-base font-medium",
+              "max-xl:hidden"
+            )}
+          >
+            {options.map((option) => (
+              <li key={option.title}>
+                <a
+                  href={option.url}
+                  className="after:content-[''] after:block after:bg-big-foot-feel after:w-0 after:h-1 hover:after:w-full after:transition-[width_3.s]"
+                >
+                  {option.title}
+                </a>
+              </li>
+            ))}
+            <button className="bg-big-foot-feel text-gray-50 px-8 py-4 font-bold rounded-md">
+              Fale comigo
+            </button>
           </ul>
-          <button className="bg-big-foot-feel text-gray-50 px-8 py-4 font-bold rounded-md">
-            Fale comigo
-          </button>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+interface MenuHamburgerProps {
+  options: Options[];
+}
+
+function MenuHamburger({ options }: MenuHamburgerProps) {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <>
+      <div
+        className={clsx("hidden", "max-xl:flex")}
+        onClick={() => setIsOpen(true)}
+      >
+        <List className="cursor-pointer hover:opacity-70" size={32} />
       </div>
-    </nav>
+      {isOpen && (
+        <div
+          className="fixed top-0 right-0 w-full h-full bg-black bg-opacity-20 animate-fade-in"
+          onClick={() => setIsOpen(false)}
+        >
+          <div
+            className="fixed top-0 right-0 h-full bg-white w-3/4
+            flex justify-start items-start p-10 py-20 animate-slide-right-to-left"
+            onClick={(e) => e.stopPropagation()}
+          >
+            <X
+              className="absolute right-5 top-10"
+              size={32}
+              onClick={() => setIsOpen(false)}
+            />
+            <ul className="flex flex-col gap-6 text-gray-700 text-base font-medium  ">
+              {options.map((option) => (
+                <li key={option.title}>
+                  <a href={option.url} onClick={() => setIsOpen(false)}>
+                    {option.title}
+                  </a>
+                </li>
+              ))}
+            </ul>
+            <button className="bg-big-foot-feel text-gray-50 px-8 py-4 font-bold rounded-md">
+              Fale comigo
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
